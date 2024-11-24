ccbb2a9fbe8f5e86d716f21edfefbf5d8cfb1ea1
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-11-24 14:56:03 +0000

    index.html working correctly

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p9-me48asmcs-p##x_zwd4y88m&^l#8ry-!0+cx^w0j@mt5ww0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-tammygirl20-boutiqueado-he3fbmrbdyi.ws.codeinstitute-ide.net','8000-tammygirl20-boutiqueado-x85j8lrb667.ws-eu116.gitpod.io', '8000-tammygirl20-boutiqueado-g5n3ziupz5p.ws.codeinstitute-ide.net/']
+ALLOWED_HOSTS = ['8000-tammygirl20-boutiqueado-he3fbmrbdyi.ws.codeinstitute-ide.net','8000-tammygirl20-boutiqueado-x85j8lrb667.ws-eu116.gitpod.io', '8000-tammygirl20-boutiqueado-g5n3ziupz5p.ws.codeinstitute-ide.net/', 'http://8000-tammygirl20-boutiqueado-he3fbmrbdyi.ws.codeinstitute-ide.net/']
 
 
 # Application definition
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,17 @@
+#from django.contrib import admin
+#from django.urls import path
+#from . import views
+
+#urlpatterns = [
+#    path('', 'http://8000-tammygirl20-boutiqueado-he3fbmrbdyi.ws.codeinstitute-ide.net/', views.home, name='home'),
+#] 
+
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path, include
+from . import views  
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', views.index, name='home'),
 ]
