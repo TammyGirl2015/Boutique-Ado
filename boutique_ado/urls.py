4147a7e6e8233cca0c8438fdec0773b73dafe3ee
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-11-25 03:52:16 +0000

    added product views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -18,11 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('profile/', views.profile, name='profile'),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,3 @@
-#from django.contrib import admin
-#from django.urls import path
-#from . import views
-
-#urlpatterns = [
-#    path('', 'http://8000-tammygirl20-boutiqueado-he3fbmrbdyi.ws.codeinstitute-ide.net/', views.home, name='home'),
-#] 
-
 from django.contrib import admin
 from django.urls import path, include
 from . import views  
