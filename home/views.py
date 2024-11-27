f1d45a3fc2eae3d06a0ba377fce3636f791963dc
Tamarie Maenzanise <mtamarie@yahoo.com>
2024-11-27 12:10:25 +0000

    added shopping bag app, urls and templates

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+# Create your views here.
+
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
 
+
 def profile(request):
-    return render(request, 'home/base.html')
+    return render(request, 'home/profile.html')
